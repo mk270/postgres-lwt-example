024b3c4a02904227c3aed173829739157b28a828
Martin Keegan <martin@no.ucant.org>
2014-05-31 22:06:42 +0100

    initial commit

---
+++ src/db.ml
@@ -0,0 +1,32 @@
+
+open Printf
+
+let (>>=) = Lwt.bind
+
+module Lwt_thread = struct
+  include Lwt
+  include Lwt_chan
+end
+
+module Lwt_PGOCaml = PGOCaml_generic.Make (Lwt_thread)
+
+let print_row row =
+	let string_of_optional = function
+		| None -> "NULL"
+        | Some str -> sprintf "%S" str
+	in
+		List.map string_of_optional row |>
+		String.concat "; " |>
+		Lwt_io.printl
+
+let init () =
+	let query = "select * from package;"
+	and user = "iatidq"
+	and name = "stmt1" (* nonce identifier of prepared statement *)
+	in
+
+	lwt dbh = Lwt_PGOCaml.connect ~user () in
+	lwt () = Lwt_PGOCaml.prepare dbh ~query ~name () in
+	lwt rows = Lwt_PGOCaml.execute dbh ~name ~params:[] () in
+        Lwt_list.iter_s print_row rows >>=
+        (fun () -> Lwt_PGOCaml.close dbh)
---
+++ src/main.ml
@@ -0,0 +1,3 @@
+
+let () = 
+	Lwt_main.run (Db.init ())
