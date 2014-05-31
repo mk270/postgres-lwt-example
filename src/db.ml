fd5a071a3dc2131b27f0d948c0b8a6e2c3c7eff1
Martin Keegan <martin@no.ucant.org>
2014-06-01 00:55:42 +0100

    be more idiomatic

--- src/db.ml
+++ src/db.ml
@@ -26,7 +26,7 @@ let init () =
 	in
 
 	lwt dbh = Lwt_PGOCaml.connect ~user () in
-	lwt () = Lwt_PGOCaml.prepare dbh ~query ~name () in
-	lwt rows = Lwt_PGOCaml.execute dbh ~name ~params:[] () in
-        Lwt_list.iter_s print_row rows >>
+	    Lwt_PGOCaml.prepare dbh ~query ~name () >>
+	    Lwt_PGOCaml.execute dbh ~name ~params:[] () >>=
+        Lwt_list.iter_s print_row >>
         Lwt_PGOCaml.close dbh
